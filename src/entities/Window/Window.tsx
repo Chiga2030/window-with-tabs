476eef7f1a3a05fc6a4248cc8dfc8c75d0f9a3cb
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-05-20 19:51:21 +0300

    updated window entity

--- src/entities/Window/Window.tsx
+++ src/entities/Window/Window.tsx
@@ -1,21 +1,34 @@
 import styles from './Window.module.scss'
 import { Menu } from './ui/Menu/Menu'
+import { AddTabButtonType } from '../../shared/types/features/AddTabButtonType'
+import { Tabs } from './ui/Tabs/Tabs'
+import { TabContent } from './ui/TabContent/TabContent'
 
 
-export const Window = () => {
+interface WindowProps {
+  actionSlots: {
+    AddTabButton: AddTabButtonType
+  }
+}
+
+export const Window = ({
+  actionSlots,
+}: WindowProps) => {
   return (
     <div className={ styles.window }>
       <div className={ styles.menu }>
-        <Menu />
+        <Menu actionSlots={ {
+          AddTabButton: actionSlots.AddTabButton
+        } } />
       </div>
 
       <div className={ styles.content }>
         <div className={ styles.tabs }>
-          tabs
+          <Tabs />
         </div>
 
         <div className={ styles.tabContent }>
-          tab content
+          <TabContent />
         </div>
       </div>
     </div>

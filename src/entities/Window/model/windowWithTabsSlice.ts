85221e442cfd176c97259e5569e634fad2fcc205
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-05-20 19:49:49 +0300

    updated slice for window

--- src/entities/Window/model/windowWithTabsSlice.ts
+++ src/entities/Window/model/windowWithTabsSlice.ts
@@ -1,42 +1,60 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { RootState } from '../../../app/store/store'
+
 
 interface Tab {
+  id: number
   name: string
   content: string
 }
 
 interface WindowState {
   tabs: Tab[]
+  opennedTabsId: number[]
+  currentTabId: number | null
 }
+
 
 const initialState: WindowState = {
   tabs: [
     {
+      id: 1,
       name: 'tab - 1',
       content: 'content tab - 1',
     },
     {
+      id: 2,
       name: 'tab - 2',
       content: 'content tab - 2',
     },
     {
+      id: 3,
       name: 'tab - 3',
       content: 'content tab - 3',
     },
   ],
+  opennedTabsId: [],
+  currentTabId: null,
 }
 
 
 export const windowWithTabsSlice = createSlice({
   name: 'windowWithTabs',
   initialState,
-  reducers: {},
+  reducers: {
+    onCreateTab: (state, action: PayloadAction<number>) => {
+      const tabIndex = state.tabs.findIndex(tab => tab.id === action.payload)
+
+      state.opennedTabsId.push(tabIndex)
+    },
+
+    onSetCurrentTabId: (state, action: PayloadAction<number>) => {
+      state.currentTabId = action.payload
+    }
+  },
 })
 
-export const { } = windowWithTabsSlice.actions
 
-export const tabs = (state: RootState) => state.windowWithTabs.tabs
+export const { onCreateTab, onSetCurrentTabId } = windowWithTabsSlice.actions
 
 export default windowWithTabsSlice.reducer

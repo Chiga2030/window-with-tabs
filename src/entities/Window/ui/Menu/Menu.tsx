322e946f1532e192214b043225c020c0b3ebe8e5
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-05-20 19:48:03 +0300

    created menu component

--- src/entities/Window/ui/Menu/Menu.tsx
+++ src/entities/Window/ui/Menu/Menu.tsx
@@ -1,10 +1,34 @@
-export const Menu = () => {
+import { useAppSelector } from "../../../../app/store/hooks"
+import styles from './Menu.module.scss'
+import { AddTabButtonType } from "../../../../shared/types/features/AddTabButtonType"
+
+
+interface MenuProps {
+  actionSlots: {
+    AddTabButton: AddTabButtonType
+  }
+}
+
+
+export const Menu = ({
+  actionSlots,
+}: MenuProps) => {
+  const tabs = useAppSelector(store => store.windowWithTabs.tabs)
+
+  const { AddTabButton } = actionSlots
+
+
   return (
-    <>
+    <div className={ styles.menu }>
       Menu
 
-      <button type={ "button" }>tab 1</button>
-      <button type={ "button" }>tab 2</button>
-    </>
+      <div className={ styles.buttonsWrapper }>
+        { tabs.map(tab => (
+          <AddTabButton key={ tab.id } tabId={ tab.id }>
+            { tab.name }
+          </AddTabButton>
+        )) }
+      </div>
+    </div>
   )
 }
